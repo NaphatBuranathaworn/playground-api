ba88549cb908a9604f6379ab8a6a8d86189c743a
NaphatB <naphat@orcsoft.co.th>
2020-06-26 17:51:06 +0700

    add config

---
+++ app.js
@@ -0,0 +1,23 @@
+var compression = require('compression')
+var express = require('express')
+const port = 3000
+var app = express()
+app.use(compression())
+
+app.get('/search', function (req, res) {
+    // Simulating async operation
+
+    console.log('dd : ', req);
+
+    setImmediate(function () {
+        var jsonStr = req.query.params
+        try {
+            var jsonObj = JSON.parse(jsonStr)
+            res.send('Success')
+        } catch (e) {
+            res.status(400).send('Invalid JSON string')
+        }
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
