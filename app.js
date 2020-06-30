5815e2ab9eecb5376846f9d4bd9d0ba2e91e25d8
NaphatB <naphat@orcsoft.co.th>
2020-06-30 15:13:52 +0700

    add route

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-var compression = require('compression')
-var express = require('express')
-var app = express()
+const compression = require('compression')
+const express = require('express')
+const app = express()
+
 app.use(compression())
-app.listen(process.env.PORT || 3000);
-
+app.listen(process.env.PORT || 3000)
 require('dotenv/config')
-console.log('Hello Node.js project.')
-console.log(process.env.MY_SECRET)
 
-app.get('/', function (req, res) {
-    res.send(JSON.stringify({ Hello: 'world' }))
-});
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const openApiDocumentation = YAML.load('./api.yaml')
 
-app.get('/search', function (req, res) {
-    console.log('dd : ', req);
-    setImmediate(function () {
-        var jsonStr = req.query.params
-        try {
-            var jsonObj = JSON.parse(jsonStr)
-            res.send('Success')
-        } catch (e) {
-            res.status(400).send('Invalid JSON string')
-        }
-    })
-})
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+
+const catalogRouter = require('./routes/catalog');
+app.use('/catalog', catalogRouter);
