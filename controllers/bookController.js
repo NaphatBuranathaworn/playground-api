42c46d116efbb3aa02847118180890fbd6b060a4
NaphatB <naphat@orcsoft.co.th>
2020-06-30 15:24:30 +0700

    change script

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,44 +1,44 @@
 
-export function index (req, res) {
+exports.index = function (req, res) {
     res.send('NOT IMPLEMENTED: Site Home Page');
-}
+};
 
 // Display list of all books.
-export function book_list(req, res) {
+exports.book_list = function(req, res) {
     res.send('NOT IMPLEMENTED: Book list');
-}
+};
 
 // Display detail page for a specific book.
-export function book_detail(req, res) {
+exports.book_detail = function(req, res) {
     res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
-}
+};
 
 // Display book create form on GET.
-export function book_create_get(req, res) {
+exports.book_create_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Book create GET');
-}
+};
 
 // Handle book create on POST.
-export function book_create_post(req, res) {
+exports.book_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Book create POST');
-}
+};
 
 // Display book delete form on GET.
-export function book_delete_get(req, res) {
+exports.book_delete_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Book delete GET');
-}
+};
 
 // Handle book delete on POST.
-export function book_delete_post(req, res) {
+exports.book_delete_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Book delete POST');
-}
+};
 
 // Display book update form on GET.
-export function book_update_get(req, res) {
+exports.book_update_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Book update GET');
-}
+};
 
 // Handle book update on POST.
-export function book_update_post(req, res) {
+exports.book_update_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Book update POST');
-}
+};
